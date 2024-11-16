a255109064d391d639f04040e1099041d4df5393
Filip Sanecki <philippo.sanecki@gmail.com>
2024-11-16 14:03:01 +0100

    Add new endpoint /chart for database display

--- src/main/java/habitTracker/MyOwnHabitTracker/controller/pageController.java
+++ src/main/java/habitTracker/MyOwnHabitTracker/controller/pageController.java
@@ -11,7 +11,10 @@ public class pageController {
     }
     @GetMapping("/")
     public String home() {
-
         return "home";
+    }
+    @GetMapping("/chart")
+    public String chart() {
+        return "chart";
     }
 }
